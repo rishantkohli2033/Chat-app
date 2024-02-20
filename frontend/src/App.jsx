b869dcc5aa366e74585a2f9ce7952508567c0a84
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-02-20 17:20:01 +0530

    routes added(frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Login from './pages/login/Login'
-// import SignUp from './pages/signup/SignUp'
-import Home from './pages/home/Home'
-function App() {
-  return       (
+import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home' 
+import { Route, Routes } from 'react-router-dom'
+
+function App(e) {
+  return( 
     <div className='p-4 h-screen flex items-center justify-center'>
-        <Home />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>} />
+      </Routes>
     </div>
   )
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import GenderCheckbox from './GenderCheckbox'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
@@ -40,12 +41,12 @@ const SignUp = () => {
 
           <GenderCheckbox/>
 
-          <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+          <Link to="/login" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
             Already have an account?
-          </a>
+          </Link>
           <div>
-            <button className='btn btn-block btn-sm mt-2'>
-              Login
+            <button className='btn btn-block btn-sm mt-2' href="/login">
+              Sign up
             </button>
           </div>
         </form>
