bd2d8ead41186693a8fc2de6b6fa7c00ae924ff9
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-02-22 01:51:42 +0530

    dynamic sidebar

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import { TiMessages } from "react-icons/ti";
 import { useAuthContext } from '../../context/AuthContext';
+import useConversation from '../../zustand/useConversation';
 const MessageContainer = () => {
-    const noChatSelected  = true;
+    const {selectedConversation, setSelectedConversation} = useConversation();
+    useEffect(() => {
+      //cleanup function
+      return () => setSelectedConversation(null);
+    },[setSelectedConversation]);
   return (
     <div className='md:min-w-[450px] flex flex-col'>
-        {noChatSelected ? <NoChatSelected/> : (
+        {!selectedConversation ? <NoChatSelected/> : (
             <>
             <div className='bg-slate-500 px-4 py-2 mb-2'>
-                <span className='label-text'>To:</span><span className='text-gray-900 font-bold'> Bhaktiman</span>
+                <span className='label-text'>To:</span><span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
             </div>
 
             <Messages/>
@@ -25,7 +30,6 @@ export default MessageContainer
 
 const NoChatSelected = () => {
 	const { authUser } = useAuthContext();
-  console.log(authUser.fullName);
 	return (
 		<div className='flex items-center justify-center w-full h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
