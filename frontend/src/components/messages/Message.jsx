5b68110d749e0459aa9676a66f48002e1fdb85a5
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-05-03 01:52:50 +0530

    message update

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -19,7 +19,7 @@ const Message = ({message}) => {
                 <img src={profilePic} alt="message image" />
             </div>
         </div>
-        <div className={`chat-bubble truncate text-white ${bubbleBgColor} ${shakeMessage} pb-2`}>
+        <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeMessage} pb-2`}>
            {message.message}
         </div>
         <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>
